c3226866a9421bdb8843bde00ecdbfe3089662c4
berecC22003 <berecC22003@sus.htl-grieskirchen.at>
2024-09-24 14:28:52 +0200

    Old for each prints added

--- bsp1/src/HalloJavamitForEach.java
+++ bsp1/src/HalloJavamitForEach.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class HalloJavamitForEach {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+    List<String> stringListOld = new ArrayList<>();
+
+    public void printOld(List<String> stringList){
+        for(String arrayList: stringList){
+            System.out.println(arrayList);
+        }
+    }
+
+
+    
 }
