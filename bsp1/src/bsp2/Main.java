0990f0b3a18e66172eb33faca578c7dcdac95f93
berecC22003 <berecC22003@sus.htl-grieskirchen.at>
2024-09-24 14:52:37 +0200

    Package created for all examples

--- bsp1/src/bsp2/Main.java
+++ bsp1/src/bsp2/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         HalloJavamitForEach halloJavamitForEach = new HalloJavamitForEach();
 
+        halloJavamitForEach.stringListOld.add("Kotlin");
+        halloJavamitForEach.stringListOld.add("Java");
+
         halloJavamitForEach.printOld();
         halloJavamitForEach.printNew();
 
